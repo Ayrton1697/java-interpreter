1b7965adddc5a36f50c1a408dd67f5488769e5c6
Ayrton1697 <ayrton.steffich@gmail.com>
2025-02-25 00:10:58 -0300

    and or

--- lox/Parser.java
+++ lox/Parser.java
@@ -104,7 +104,7 @@ class Parser {
     }
 
     private Expr assignment(){
-        Expr expr = equality();
+        Expr expr = or();
         if(match(EQUAL)){
             Token equals = previous();
             Expr value = assignment();
@@ -117,6 +117,26 @@ class Parser {
             error(equals, "Invalid assignment target.");
         }
 
+        return expr;
+    }
+
+    private Expr or(){
+        Expr expr = and();
+        while(match(OR)){
+            Token operator = previous();
+            Expr right = and();
+            expr = new Expr.Logical(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr and(){
+        Expr expr = equality();
+        while(match(AND)){
+            Token oeprator = previous();
+            Expr right = equality();
+            expr = new Expr.Logical(expr, oeprator, right);
+        }
         return expr;
     }
 
