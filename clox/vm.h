f0ce224bd5600f19a48757f412887d9da824db65
Ayrton1697 <ayrton.steffich@gmail.com>
2025-06-24 00:00:16 -0300

    chap 15

--- clox/vm.h
+++ clox/vm.h
@@ -2,6 +2,7 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
 
 #define STACK_MAX 256
 
@@ -21,5 +22,7 @@ typedef enum {
 void initVM();
 void freeVM();
 InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
 
 #endif
