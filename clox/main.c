7ddaa66c5480dedeff9cf13de8b35730b68999e6
Ayrton1697 <ayrton.steffich@gmail.com>
2025-06-26 00:21:36 -0300

    scanner start

--- clox/main.c
+++ clox/main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
@@ -7,31 +11,14 @@ int main(int argc, const char* argv[]){
     
     initVM();
     
-    Chunk chunk;
-    initChunk(&chunk);
-
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-
-    writeChunk(&chunk, OP_NEGATE, 123);
-    writeChunk(&chunk, OP_RETURN, 123);
-
-    dissasembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
+    if(argc == 1){
+        repl();
+    } else if (argc == 2){
+        runfile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: clox [path]'n");
+        exit(64);
+    }
     freeVM();
-    freeChunk(&chunk);
     return 0;
 }
