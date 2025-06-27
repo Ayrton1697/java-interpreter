45b3e29a4a4a4227fe8ef22d5e06c4ff2d638b5f
Ayrton1697 <ayrton.steffich@gmail.com>
2025-06-27 00:25:10 -0300

    tokens in compiler

--- clox/scanner.h
+++ clox/scanner.h
@@ -1,6 +1,18 @@
 #ifndef clox_scanner_h
 #define clox_scanner_h
 
-void initScanner(const char* source);
+typedef enum {
+    //  Single character tokens
+    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+} TokenType;
 
+typedef struct {
+    int line;
+    int length;
+    const char* start;
+    TokenType type;
+} Token;
+
+void initScanner(const char* source);
+Token scanToken();
 #endif
