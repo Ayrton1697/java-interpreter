842478e4d34c3c37a4d0fc47db4ecbae0a524720
Ayrton1697 <ayrton.steffich@gmail.com>
2025-06-26 23:18:11 -0300

    file reading

--- clox/main.c
+++ clox/main.c
@@ -19,6 +19,31 @@ static void repl(){
  }
 }
 
+static char* readFile(const char* path){
+    FILE* file = fopen(path, "rb");
+
+    if(file == NULL){
+        fprintf(stderr,"Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(fileSize +1);
+    if(buffer == NULL){
+        fprintf(stderr, "Not enough memory tor read \"%s\".\n", path);
+    }
+    size_t bytesRead = fred(buffer,sizeof(char),fileSize, file);
+    if(bytesRead < fileSize){
+        fprintf(stderr,"Could not read file \"%s\".\n",path);
+    }
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
 static void runFile(const char* path){
     char* source = readFile(path);
     InterpretResult result = interpret(source);
