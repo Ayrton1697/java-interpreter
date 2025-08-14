2f62ec33d84905d0266243d6e84d4cfa31d41665
Ayrton1697 <ayrton.steffich@gmail.com>
2025-08-13 21:24:16 -0300

    hash table deleting

--- clox/table.c
+++ clox/table.c
@@ -21,9 +21,17 @@ void freeTable(Table* table){
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key){
     uint32_t index = key->hash % capacity;
+    Entry* tombstone = NULL;
     for(;;){
         Entry* entry = &entries[index];
-        if(entry->key == key || entry->key == NULL){
+        if(entry->key == NULL){
+            if(IS_NIL(entry->value)){
+                // empty entry
+                return tombstone != NULL ? tombstone : entry;
+            } else {
+                if(tombstone == NULL) tombstone = entry;
+            } 
+        }  else if(entry->key == key) {
             return entry;
         }
 
@@ -75,7 +83,16 @@ bool tableSet(Table* table, ObjString* key, Value value){
     entry->key = key;
     entry->value = value;
     return isNewKey;
-}   
+}
+
+bool tableDelete(Table* table, ObjString* key){
+    if(table->count == 0) return false;
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    if(entry == NULL) return false;
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+    return true;
+}
 
 void tableAddAll(Table* from, Table* to){
     for(int i = 0; i < from->capacity; i++){
