f4fab56c92f1c281bc3cd0db7a2470bbdf9f941c
Ayrton1697 <ayrton.steffich@gmail.com>
2025-09-14 23:18:23 -0300

    functions

--- clox/compiler.c
+++ clox/compiler.c
@@ -46,7 +46,15 @@ typedef struct {
     int depth;
 } Local;
 
+typedef struct{
+    TYPE_FUNCTION,
+    TYPE_SCRIPT
+} FunctionType;
+
 typedef struct {
+    ObjFunction* function;
+    FunctionType type;
+
     Local locals[UINT8_COUNT];
     int localCount;
     int scopeDepth;
@@ -54,10 +62,9 @@ typedef struct {
 
 Parser parser;
 Compiler* current = NULL;
-Chunk* compilingChunk;
 
 static Chunk* currentChunk(){
-    return compilingChunk;
+    return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message){
@@ -167,10 +174,18 @@ static void patchJump(int offset){
     currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler){
+static void initCompiler(Compiler* compiler, FunctionType type){
+    compiler->function = NULL;
+    compiler->type = type;
     compiler->localCount = 0;
     compiler->scopeDepth = 0;
+    compiler->function = newFunction();
     current = compiler;
+
+    Local* local = &current->locals[current->localCount++];
+    local->depth = 0;
+    local->name.start = "";
+    local->name.length = 0;
 }
 
 static void endCompiler(){
@@ -606,8 +621,8 @@ static ParseRule* getRule(TokenType type){
 bool compile(const char* source, Chunk* chunk){
     initScanner(source);
     Compiler compiler;
-    initCompiler(&compiler);
-    compilingChunk = chunk;
+    initCompiler(&compiler, TYPE_SCRIPT);
+
     parser.hadError = false;
     parser.panicMode = false;
     // int line = -1;
