16146ac9e1de5ff14db64e98cc30079617805a92
Ayrton1697 <ayrton.steffich@gmail.com>
2025-10-23 00:05:14 -0300

    upvalues

--- clox/compiler.c
+++ clox/compiler.c
@@ -44,6 +44,7 @@ typedef struct {
 typedef struct {
     Token name;
     int depth;
+    bool isCaptured;
 } Local;
 
 typedef struct{
@@ -197,6 +198,7 @@ static void initCompiler(Compiler* compiler, FunctionType type){
     }
     Local* local = &current->locals[current->localCount++];
     local->depth = 0;
+    local->isCaptured = false;
     local->name.start = "";
     local->name.length = 0;
 }
@@ -221,7 +223,12 @@ static void beginScope(){
 static void endScope(){
     current->scopeDepth--;
     while(current->localCount > 0 && current->locals[current->localCount -1].depth > current->scopeDepth){
-        emitByte(OP_POP);
+        if(current->locals[current->localCount - 1].isCaptured){
+            emitByte(OP_CLOSE_UPVALUE);
+        } else {
+            emitByte(OP_POP);
+        }
+    
         current->localCount--;
     }
 }
@@ -245,7 +252,8 @@ static void addLocal(Token name){
     Local* local = &current->locals[current->localCount++];
     local->name = name;
     local->depth = -1;
-    local->depth = current->scopeDepth;
+    // local->depth = current->scopeDepth;
+    local->isCaptured = false;
 }
 
 static bool identifiersEqual(Token* a, Token*b){
@@ -290,6 +298,7 @@ static int resolveUpvalue(Compiler* compiler, Token* name){
 
     int local = resolveLocal(compiler->enclosing, name);
     if(local != -1){
+        compiler->enclosing->locals[local].isCaptured = true;
         return addUpvalue(compiler, (uint8_t)local,true);
     }
     int upvalue = resolveUpvalue(compiler->enclosing, name);
