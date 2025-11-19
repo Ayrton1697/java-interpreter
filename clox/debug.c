d1de239f8411e1fb35f7934639841d1e7f08daa7
Ayrton1697 <ayrton.steffich@gmail.com>
2025-11-18 23:45:07 -0300

    chap27classes

--- clox/debug.c
+++ clox/debug.c
@@ -123,6 +123,8 @@ int dissasembleInstruction(Chunk* chunk, int offset){
         return simpleInstruction("OP_CLOSE_UPVALUE",offset);
     case OP_RETURN:
         return simpleInstruction("OP_PRINT", offset);
+    case OP_CLASS:
+        return constantInstruction("OP_CLASS", chunk,  offset);
     default:
         printf("Unknown opcode %d\n", instruction);
         return offset +1;
